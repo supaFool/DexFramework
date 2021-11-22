b904dedbeb9caf9b6d45265cabfff7161f72b16c
supaFool <supafool@live.com>
2021-11-22 02:54:05 -0500

    Added login.js to use instead of entire file.

--- src/login.js
+++ src/login.js
@@ -1,32 +1,3 @@
-const serverUrl = "https://klpxwezyuiua.usemoralis.com:2053/server"; //Server url from moralis.io
-const appId = "SJlvpL4zKheiaYlFnUpSL5ozOZsq7D7Q45nbwckX"; // Application id from moralis.io
-
-
-//keeps track of if a user is logged in.
-let logged_in;
-
-//Called when site is loading.
-async function init() {
-    await Moralis.start({ serverUrl, appId });
-    await Moralis.enableWeb3();
-
-    token_obj = await Moralis.Web3API.token;
-    currentUser = Moralis.User.current();
-
-    //document.getElementById("slippage").value = slippage;
-    //If User is logged in
-    if (currentUser) {
-        logged_in = true;
-        document.getElementById("login_button").innerText = "Logout";
-    }
-
-    //If user is not logged in
-    else {
-        logged_in = false;
-        document.getElementById("login_button").innerText = "Sign in with Metamask";
-    }
-}
-
 async function login() {
     try {
         currentUser = Moralis.User.current();
@@ -53,5 +24,4 @@ async function logOut() {
     logged_in = false;
 }
 
-init();
 document.getElementById("login_button").onclick = login;

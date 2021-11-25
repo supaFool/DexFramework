476b3ddadb7b7f5ac001e2232c3ccc2827ce627b
Colten A Wills <45346350+ctech1313@users.noreply.github.com>
2021-11-25 03:28:15 -0500

    test

--- src/login.js
+++ src/login.js
@@ -25,3 +25,31 @@ async function logOut() {
 }
 
 document.getElementById("login_button").onclick = login;
+
+async function loginWC() {
+    try {
+        currentUser = Moralis.User.current();
+        if (!currentUser) {
+            document.getElementById("login_button_wc").innerText = "Authenticating...";
+            currentUser = await Moralis.authenticate({ provider: "walletconnect", chainId: 56 })
+
+        } else {
+            logOut();
+        }
+        document.getElementById("login_button_wc").innerText = "Logout";
+        logged_in = true;
+    } catch (error) {
+        if (error.message == "MetaMask Message Signature: User denied message signature.") {
+            alert("Login cancelled")
+            document.getElementById("login_button_wc").innerText = "Sign in with WalletConnect";
+        }
+    }
+}
+async function logOut() {
+    currentUser = await Moralis.User.logOut();
+    document.getElementById("login_button_wc").innerText = "Sign in with WalletConnect";
+
+    logged_in = false;
+}
+
+document.getElementById("login_button_wc").onclick = loginWC;
--- src/main.js
+++ src/main.js
@@ -39,8 +39,10 @@ async function init() {
         logged_in = true;
         document.getElementById("swap_button").disabled = false;
         document.getElementById("login_button").innerText = "Logout";
+        document.getElementById("login_button_wc").innerText = "Logout";
         setHelperData();
         console.log(global.user_profile.born);
+        console.log(logged_in);
     }
 
     //If user is not logged in
@@ -48,6 +50,8 @@ async function init() {
         logged_in = false;
         document.getElementById("swap_button").disabled = true;
         document.getElementById("login_button").innerText = "Sign in with Metamask";
+        document.getElementById("login_button_wc").innerText = "Sign in with WalletConnect";
+        console.log(logged_in);
     }
 }
 
@@ -398,6 +402,65 @@ function openSidebar() {
 }
 
 init();
+
+async function login() {
+    try {
+        currentUser = Moralis.User.current();
+        if (!currentUser) {
+            document.getElementById("login_button").innerText = "Authenticating...";
+            document.getElementById("swap_button").disabled = true;
+            console.log(logged_in);
+            currentUser = await Moralis.authenticate();
+        } else {
+            document.getElementById("swap_button").disabled = false;
+            console.log(logged_in);
+            logOut();
+        }
+        document.getElementById("login_button").innerText = "Logout";
+        logged_in = true;
+    } catch (error) {
+        if (error.message == "MetaMask Message Signature: User denied message signature.") {
+            alert("Login cancelled")
+            document.getElementById("login_button").innerText = "Sign in with Metamask";
+        }
+    }
+}
+async function logOut() {
+    currentUser = await Moralis.User.logOut();
+    document.getElementById("login_button").innerText = "Sign in with Metamask";
+
+    logged_in = false;
+}
+
+document.getElementById("login_button").onclick = login;
+
+async function loginWC() {
+    try {
+        currentUser = Moralis.User.current();
+        if (!currentUser) {
+            document.getElementById("login_button_wc").innerText = "Authenticating...";
+            currentUser = await Moralis.authenticate({ provider: "walletconnect", chainId: 56 })
+
+        } else {
+            logOut();
+        }
+        document.getElementById("login_button_wc").innerText = "Logout";
+        logged_in = true;
+    } catch (error) {
+        if (error.message == "MetaMask Message Signature: User denied message signature.") {
+            alert("Login cancelled")
+            document.getElementById("login_button_wc").innerText = "Sign in with WalletConnect";
+        }
+    }
+}
+async function logOut() {
+    currentUser = await Moralis.User.logOut();
+    document.getElementById("login_button_wc").innerText = "Sign in with WalletConnect";
+
+    logged_in = false;
+}
+
+document.getElementById("login_button_wc").onclick = loginWC;
 
 document.getElementById("modal_close").onclick = closeModal;
 document.getElementById("from_token_select").onclick = () => {
